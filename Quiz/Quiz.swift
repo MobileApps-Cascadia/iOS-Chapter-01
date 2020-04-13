297d5b586c7f09ed8ef45dd3d13157836a06917c
Clay Suttner <csuttner@gmail.com>
2020-04-12 17:04:27 -0700

    Adds missing IBAction connections to storyboard and updates VC code to work with Quiz class

--- Quiz/Quiz.swift
+++ Quiz/Quiz.swift
@@ -8,27 +8,27 @@
 
 class Quiz{
  
-private let questions: [String] = [
-    "From what is cognac made?",
-    "What is 7+7?",
-    "What is the capital of Vermont?"
-]
-    
-private let answers: [String] = [
-    "Grapes",
-    "14",
-    "Montpelier"
-]
+    private let questions: [String] = [
+        "From what is cognac made?",
+        "What is 7+7?",
+        "What is the capital of Vermont?"
+    ]
+        
+    private let answers: [String] = [
+        "Grapes",
+        "14",
+        "Montpelier"
+    ]
 
-    var count : Int {
+    var count: Int {
         return questions.count
     }
     
-    func question( number num:Int ) -> String {
+    func question(number num:Int) -> String {
         return questions[num]
     }
     
-    func answer( number num:Int ) -> String {
+    func answer(number num:Int) -> String {
         return answers[num]
     }
 }
